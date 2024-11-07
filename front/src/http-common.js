012b9d02973835e15b45f2dca5389b790fa147c1
maxito <cesar.rodriguez.pa@usach.cl>
2024-11-07 01:11:59 -0300

    Cambios

--- front/src/http-common.js
+++ front/src/http-common.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
-//const backendServer = "104.41.56.72";
-//const backendPort = import.meta.env.VITE_BACKEND_PORT;
+const backendServer = "104.41.56.72";
+const backendPort = import.meta.env.VITE_BACKEND_PORT;
 
 //const backendServer = import.meta.env.VITE_BACKEND_SERVER;
 //const backendPort = import.meta.env.VITE_BACKEND_PORT;
@@ -9,7 +9,7 @@ import axios from "axios";
 //console.log(backendServer, backendPort);
 
 export default axios.create({
-    baseURL: `http://localhost:8090`,
+    baseURL: `http://${backendServer}:${backendPort}`,
     headers: {
         'Content-Type': 'application/json'
     }
