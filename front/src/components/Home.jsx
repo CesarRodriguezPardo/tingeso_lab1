5baca093b5b0075cde5b77bd9f7bdc2153e04280
maxito <cesar.rodriguez.pa@usach.cl>
2024-11-07 15:36:24 -0300

    Cambio 1

--- front/src/components/Home.jsx
+++ front/src/components/Home.jsx
@@ -4,6 +4,7 @@ const Home = () => {
       <h1>PrestaBanco: Credit service</h1>
       <p>
   PrestaBanco is a web application for managing loans. This application has been developed using technologies such as 
+  benjita
   <a href="https://spring.io/projects/spring-boot">Spring Boot</a> (for the backend), 
   <a href="https://reactjs.org/">React</a> (for the frontend), and 
   <a href="https://www.mysql.com/products/community/">MySQL</a> (for the database).
